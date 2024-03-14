1bbee4f119d9ab87bc7f3bd925ebef2d6890a4f3
Heri Setyawan <herisetyawan233@gmail.com>
2024-03-14 18:56:13 +0700

    refactor: consume asset admin

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,43 @@
+<!--**********************************
+    Sidebar start
+***********************************-->
+<div class="quixnav">
+    <div class="quixnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label first">Main Menu</li>
+            <li>
+                <a class="has-arrows" href="/">
+                    <i class="icon icon-app-store"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a class="has-arrows" href="{{ route('mahasiswa.index') }}">
+                    <i class="icon icon-app-store"></i>
+                    <span class="nav-text">Mahasiswa</span>
+                </a>
+            </li>
+            <li>
+                <a class="has-arrows" href="/">
+                    <i class="icon icon-app-store"></i>
+                    <span class="nav-text">Alternatif</span>
+                </a>
+            </li>
+            <li>
+                <a class="has-arrows" href="/">
+                    <i class="icon icon-app-store"></i>
+                    <span class="nav-text">Kriteria</span>
+                </a>
+            </li>  
+            <li>
+                <a class="has-arrows" href="/">
+                    <i class="icon icon-app-store"></i>
+                    <span class="nav-text">Profile</span>
+                </a>
+            </li> 
+        </ul>
+    </div>
+</div>
+<!--**********************************
+    Sidebar end
+***********************************-->
